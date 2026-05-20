a8007b27e89e862efcc4ec1d346b0944bed29ace
drd1201 <drd1201@gmail.com>
2026-05-20 13:53:56 -0500

    Remove notifyHandler from worker.js
    Remove the notifyHandler and related fetch logic.

--- worker.js
+++ worker.js
@@ -1,14 +1,1 @@
-import notifyHandler from "./functions/api/notify.js";
 
-export default {
-  async fetch(request, env, ctx) {
-    const url = new URL(request.url);
-    
-    if (url.pathname === "/api/notify") {
-      return notifyHandler.fetch(request, env, ctx);
-    }
-
-    // Serve static files for everything else
-    return fetch(request);
-  }
-};
